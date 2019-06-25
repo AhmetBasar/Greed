4d4337efd00856c4f9e5a48fe97089cac1f44530
AhmetBasar <1989ahmetbasar@gmail.com>
2019-06-25 21:08:46 +0300

    Testing Framework.

--- Greed/test/chess/engine/test/BitboardUtilityTest.java
+++ Greed/test/chess/engine/test/BitboardUtilityTest.java
@@ -1,5 +1,137 @@
 package chess.engine.test;
 
+import chess.debug.DebugUtility;
+import chess.engine.EngineConstants;
+import chess.engine.Transformer;
+import chess.evaluation.BitboardUtility;
+
 public class BitboardUtilityTest {
 
+	public static void main(String[] args) {
+		
+		testAll();
+		
+		byte [][] EMPTY_BOARD2={
+				   {     0,     0,     0,     2,     0,     0,     0,     2},
+				   {     0,     0,     0,     0,     2,     0,     0,     2},
+				   {     0,     0,     0,     0,     0,     2,     0,     0},
+				   {     2,     0,     0,     0,     0,     0,     0,     0},
+				   {     0,     0,     0,     0,     0,     0,     0,     0},
+				   {     0,     0,     0,     0,     0,     0,     0,     0},
+				   {     0,     2,     0,     0,     0,     0,     0,     0},
+				   {     2,     0,     0,     0,     0,     0,     0,     2}
+		};
+		
+		byte[][] board2 = new byte[8][8];
+		for(int x=0;x<=7;x++){
+			for(int y=0;y<=7;y++){
+				board2[x][y]=EMPTY_BOARD2[y][x];
+			}
+		}
+		
+		long[] emptyBb1 = Transformer.getBitboardStyl(board2);
+		
+		System.out.println("wp = " + emptyBb1[EngineConstants.WHITE_PAWN]);
+		
+		
+		long[] bbb = DebugUtility.convertToBitboard(BitboardUtility.bEastAttackFrontSpans(emptyBb1[EngineConstants.WHITE_PAWN]), EngineConstants.WHITE_PAWN);
+		
+		System.out.println("bbb = " + bbb[EngineConstants.WHITE_PAWN]);
+		
+		DebugUtility.throwBoard(bbb);
+		
+		
+//		testAll();
+	}
+
+	public static void testAll() {
+		testWFrontSpans();
+		
+		testBFrontSpans();
+		
+		testNorthFill();
+		
+		testSouthFill();
+		
+		testNorthOne();
+		
+		testSouthOne();
+		
+		testEastOne();
+		
+		testWestOne();
+		
+		testFileFill();
+		
+		testWEastAttackFrontSpans();
+		
+		testWWestAttackFrontSpans();
+	}
+	
+	public static void testWFrontSpans() {
+		if (BitboardUtility.wFrontSpans(-8606343699237961087L) != -5502409718846226176L) {
+			throw new RuntimeException("Failed.");			
+		}
+	}
+	
+	public static void testBFrontSpans() {
+		if (BitboardUtility.bFrontSpans(-8606343699237961087L) != 38448515990010299L) {
+			throw new RuntimeException("Failed.");			
+		}
+	}
+	
+	public static void testNorthFill() {
+		if (BitboardUtility.northFill(-8606343699237961087L) != -4921410182543342719L) {
+			throw new RuntimeException("Failed.");			
+		}
+	}
+	
+	public static void testSouthFill() {
+		if (BitboardUtility.southFill(-8606343699237961087L) != -8603923980266914885L) {
+			throw new RuntimeException("Failed.");			
+		}
+	}
+	
+	public static void testNorthOne() {
+		if (BitboardUtility.northOne(-8606343699237961087L) != -8061442233481395968L) {
+			throw new RuntimeException("Failed.");			
+		}
+	}
+	
+	public static void testSouthOne() {
+		if (BitboardUtility.southOne(-8606343699237961087L) != 38439063962779650L) {
+			throw new RuntimeException("Failed.");			
+		}
+	}
+	
+	public static void testEastOne() {
+		if (BitboardUtility.eastOne(-8606343699237961087L) != 1161999081195701250L) {
+			throw new RuntimeException("Failed.");			
+		}
+	}
+	
+	public static void testWestOne() {
+		if (BitboardUtility.westOne(-8606343699237961087L) != 4920200185088311616L) {
+			throw new RuntimeException("Failed.");			
+		}
+	}
+	
+	public static void testFileFill() {
+		if (BitboardUtility.fileFill(-8606343699237961087L) != -4919131752989213765L) {
+			throw new RuntimeException("Failed.");			
+		}
+	}
+	
+	public static void testWEastAttackFrontSpans() {
+		if (BitboardUtility.wEastAttackFrontSpans(-8606343699237961087L) != 7369584463179022848L) {
+			throw new RuntimeException("Failed.");			
+		}
+	}
+	
+	public static void testWWestAttackFrontSpans() {
+		if (BitboardUtility.wWestAttackFrontSpans(-8606343699237961087L) != 6435997091012624384L) {
+			throw new RuntimeException("Failed.");			
+		}
+	}
+	
 }
