42621eebf726a3b88cc7582afc8e987d6c1d63b4
AhmetBasar <1989ahmetbasar@gmail.com>
2019-09-28 18:34:28 +0300

    Restructuring.

---
+++ Greed/src/chess/fhv2/TimeController.java
@@ -0,0 +1,68 @@
+/**********************************************
+ * Greed, a chess engine written in java.
+ * Copyright (C) 2019 Ahmet Baþar
+ * 
+ * This file is part of Greed.
+ * 
+ * Greed is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Greed is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Greed.  If not, see <https://www.gnu.org/licenses/>.
+ **********************************************/
+package chess.fhv2;
+public class TimeController implements Runnable {
+	
+	private volatile boolean suspended = true;
+	private ICallBackTimeout callBack;
+	private int checkPeriod = 10;
+	
+	public TimeController(ICallBackTimeout callBack) {
+		this.callBack = callBack;
+	}
+	
+	@Override
+	public void run() {
+		waitIfSuspended();
+		while (true) {
+			try {
+				Thread.sleep(checkPeriod);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			if (callBack.evaluateTimeoutCondition()) {
+				callBack.onTimeout();
+				suspend();
+				waitIfSuspended();
+			}
+		}
+	}
+	
+	private void waitIfSuspended() {
+		synchronized (this) {
+			while (suspended) {
+				try {
+					wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public synchronized void suspend() {
+		suspended = true;
+	}
+
+	public synchronized void resume() {
+		suspended = false;
+		notify();
+	}
+}
