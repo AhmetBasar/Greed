14ea9174b131358606890fbaf386c5ad8dd53d81
AhmetBasar <1989ahmetbasar@gmail.com>
2019-11-10 13:32:56 +0300

    Pawn HashTable Implementation.

--- Greed/src/chess/evaluation/EvaluationAdvancedV4.java
+++ Greed/src/chess/evaluation/EvaluationAdvancedV4.java
@@ -20,6 +20,9 @@
 package chess.evaluation;
 
 import chess.engine.EngineConstants;
+import chess.engine.PawnHashTable;
+import chess.engine.PawnTranspositionElement;
+import chess.engine.TranspositionTable;
 import chess.gui.GuiConstants;
 
 public class EvaluationAdvancedV4 {
@@ -37,36 +40,45 @@ public class EvaluationAdvancedV4 {
 	private static final boolean usePsqt = true;
 	private static final boolean useBishopPair = true;
 	
-	public static int evaluate(long[] bitboard, byte[][] castlingRights, int side){
-		int eval = 0;
+	public static int evaluate(long[] bitboard, byte[][] castlingRights, int side, long pawnZobristKey, PawnHashTable pawnHashTable){
 		
-		int whiteTotalPieceValue = 0;
-		int blackTotalPieceValue = 0;
+		// TODO COMMENT OUT HERE.
+		// TODO COMMENT OUT HERE.
+		// TODO COMMENT OUT HERE.
+		// TODO COMMENT OUT HERE.
+		if (pawnZobristKey != TranspositionTable.getPawnZobristKey(bitboard)) {
+			throw new RuntimeException("PATLADI");
+		}
+		
+		int eval = 0;
 		
 		long wp = bitboard[EngineConstants.WHITE_PAWN];
 		long bp = bitboard[EngineConstants.BLACK_PAWN];
 		
-		int wpCount = Long.bitCount(wp);
+		PawnTranspositionElement ttElement = pawnHashTable.probe(pawnZobristKey);
+		if(ttElement != null && ttElement.zobristKey == pawnZobristKey){
+			eval += ttElement.score;
+		} else {
+			int pawnScore = evalPawn(bitboard, wp, bp);
+			eval += pawnScore;
+			pawnHashTable.recordTranspositionTable(pawnZobristKey, pawnScore);
+		}
+		
+		int whiteTotalPieceValue = 0;
+		int blackTotalPieceValue = 0;
+		
+		
 		int wkCount = Long.bitCount(bitboard[EngineConstants.WHITE_KNIGHT]);
 		int wbCount = Long.bitCount(bitboard[EngineConstants.WHITE_BISHOP]);
 		int wrCount = Long.bitCount(bitboard[EngineConstants.WHITE_ROOK]);
 		int wqCount = Long.bitCount(bitboard[EngineConstants.WHITE_QUEEN]);
 		
-		int bpCount = Long.bitCount(bp);
 		int bkCount = Long.bitCount(bitboard[EngineConstants.BLACK_KNIGHT]);
 		int bbCount = Long.bitCount(bitboard[EngineConstants.BLACK_BISHOP]);
 		int brCount = Long.bitCount(bitboard[EngineConstants.BLACK_ROOK]);
 		int bqCount = Long.bitCount(bitboard[EngineConstants.BLACK_QUEEN]);
 
-		/**
-		 * Passed pawn bonus.
-		 **/
-		eval += (Long.bitCount(PassedPawn.whitePassedPawns(wp, bp)) - Long.bitCount(PassedPawn.blackPassedPawns(bp, wp))) * BONUS_PASSED_PAWN;
-		
-		/**
-		 * Penalty backward pawns 
-		 **/
-		eval -= (Long.bitCount(BackwardPawn.whiteBackwardPawns(wp, bp)) - Long.bitCount(BackwardPawn.blackBackwardPawns(bp, wp))) * PENALTY_BACKWARD_PAWN;
+
 		
 		whiteTotalPieceValue = wkCount * EngineConstants.WHITE_KNIGHT_V + wbCount * EngineConstants.WHITE_BISHOP_V + wrCount * EngineConstants.WHITE_ROOK_V + wqCount * EngineConstants.WHITE_QUEEN_V;
 		blackTotalPieceValue = bkCount * EngineConstants.BLACK_KNIGHT_V + bbCount * EngineConstants.BLACK_BISHOP_V + brCount * EngineConstants.BLACK_ROOK_V + bqCount * EngineConstants.BLACK_QUEEN_V;
@@ -75,20 +87,8 @@ public class EvaluationAdvancedV4 {
 		long blackPawns = bitboard[EngineConstants.BLACK_PAWN];
 		
 		int trailingZeros;
-		long fromBitboard=bitboard[EngineConstants.WHITE_PAWN];
-		while (fromBitboard != 0) {
-			trailingZeros = Long.numberOfTrailingZeros(fromBitboard);
-			
-			// Isolated Pawns
-			if ((EngineConstants.neighborFiles[trailingZeros] & whitePawns) == 0) {
-				eval -= PENALTY_ISOLATED_PAWN;
-			}
-			
-			if (usePsqt) {
-				eval += PieceSquareTable.positionalValue[EngineConstants.WHITE_PAWN][trailingZeros];
-			}
-			fromBitboard &= (fromBitboard - 1);
-		}
+		long fromBitboard;
+
 		
 		fromBitboard=bitboard[EngineConstants.WHITE_KNIGHT];
 		while (fromBitboard != 0) {
@@ -133,22 +133,6 @@ public class EvaluationAdvancedV4 {
 			trailingZeros = Long.numberOfTrailingZeros(fromBitboard);
 			if (usePsqt) {
 				eval += PieceSquareTable.positionalValue[EngineConstants.WHITE_QUEEN][trailingZeros];
-			}
-			fromBitboard &= (fromBitboard - 1);
-		}
-		
-		fromBitboard=bitboard[EngineConstants.BLACK_PAWN];
-		while (fromBitboard != 0) {
-			trailingZeros = Long.numberOfTrailingZeros(fromBitboard);
-			
-			
-			// Isolated Pawns
-			if ((EngineConstants.neighborFiles[trailingZeros] & blackPawns) == 0) {
-				eval += PENALTY_ISOLATED_PAWN;
-			}
-			
-			if (usePsqt) {
-				eval += PieceSquareTable.positionalValue[EngineConstants.BLACK_PAWN][trailingZeros];
 			}
 			fromBitboard &= (fromBitboard - 1);
 		}
@@ -269,45 +253,99 @@ public class EvaluationAdvancedV4 {
 			}
 		}
 		
-		int pawnCount = Long.bitCount(whitePawns & EngineConstants.FILE_A);
-		eval -= (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
-		pawnCount = Long.bitCount(whitePawns & EngineConstants.FILE_B);
-		eval -= (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
-		pawnCount = Long.bitCount(whitePawns & EngineConstants.FILE_C);
-		eval -= (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
-		pawnCount = Long.bitCount(whitePawns & EngineConstants.FILE_D);
-		eval -= (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
-		pawnCount = Long.bitCount(whitePawns & EngineConstants.FILE_E);
-		eval -= (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
-		pawnCount = Long.bitCount(whitePawns & EngineConstants.FILE_F);
-		eval -= (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
-		pawnCount = Long.bitCount(whitePawns & EngineConstants.FILE_G);
-		eval -= (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
-		pawnCount = Long.bitCount(whitePawns & EngineConstants.FILE_H);
-		eval -= (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
-		
-		pawnCount = Long.bitCount(blackPawns & EngineConstants.FILE_A);
-		eval += (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
-		pawnCount = Long.bitCount(blackPawns & EngineConstants.FILE_B);
-		eval += (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
-		pawnCount = Long.bitCount(blackPawns & EngineConstants.FILE_C);
-		eval += (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
-		pawnCount = Long.bitCount(blackPawns & EngineConstants.FILE_D);
-		eval += (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
-		pawnCount = Long.bitCount(blackPawns & EngineConstants.FILE_E);
-		eval += (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
-		pawnCount = Long.bitCount(blackPawns & EngineConstants.FILE_F);
-		eval += (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
-		pawnCount = Long.bitCount(blackPawns & EngineConstants.FILE_G);
-		eval += (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
-		pawnCount = Long.bitCount(blackPawns & EngineConstants.FILE_H);
-		eval += (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
+
 		
 
-		return  eval + EngineConstants.WHITE_PAWN_V 	* (wpCount - bpCount) + 
-				   		EngineConstants.WHITE_KNIGHT_V 	* (wkCount - bkCount) +
+		return  eval + EngineConstants.WHITE_KNIGHT_V 	* (wkCount - bkCount) +
 				   		EngineConstants.WHITE_BISHOP_V 	* (wbCount - bbCount) +
 				   		EngineConstants.WHITE_ROOK_V 	* (wrCount - brCount) +
 				   		EngineConstants.WHITE_QUEEN_V 	* (wqCount - bqCount);
 	}
+	
+	public static int evalPawn(long[] bitboard, long wp, long bp) {
+		int wpCount = Long.bitCount(wp);
+		int bpCount = Long.bitCount(bp);
+		int eval = 0;
+		
+		/**
+		 * Passed pawn bonus.
+		 **/
+		eval += (Long.bitCount(PassedPawn.whitePassedPawns(wp, bp)) - Long.bitCount(PassedPawn.blackPassedPawns(bp, wp))) * BONUS_PASSED_PAWN;
+		
+		/**
+		 * Penalty backward pawns 
+		 **/
+		eval -= (Long.bitCount(BackwardPawn.whiteBackwardPawns(wp, bp)) - Long.bitCount(BackwardPawn.blackBackwardPawns(bp, wp))) * PENALTY_BACKWARD_PAWN;
+		
+		int trailingZeros;
+		long fromBitboard=bitboard[EngineConstants.WHITE_PAWN];
+		while (fromBitboard != 0) {
+			trailingZeros = Long.numberOfTrailingZeros(fromBitboard);
+			
+			// Isolated Pawns
+			if ((EngineConstants.neighborFiles[trailingZeros] & wp) == 0) {
+				eval -= PENALTY_ISOLATED_PAWN;
+			}
+			
+			if (usePsqt) {
+				eval += PieceSquareTable.positionalValue[EngineConstants.WHITE_PAWN][trailingZeros];
+			}
+			fromBitboard &= (fromBitboard - 1);
+		}
+		
+		fromBitboard=bitboard[EngineConstants.BLACK_PAWN];
+		while (fromBitboard != 0) {
+			trailingZeros = Long.numberOfTrailingZeros(fromBitboard);
+			
+			
+			// Isolated Pawns
+			if ((EngineConstants.neighborFiles[trailingZeros] & bp) == 0) {
+				eval += PENALTY_ISOLATED_PAWN;
+			}
+			
+			if (usePsqt) {
+				eval += PieceSquareTable.positionalValue[EngineConstants.BLACK_PAWN][trailingZeros];
+			}
+			fromBitboard &= (fromBitboard - 1);
+		}
+		
+		int pawnCount = Long.bitCount(wp & EngineConstants.FILE_A);
+		eval -= (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
+		pawnCount = Long.bitCount(wp & EngineConstants.FILE_B);
+		eval -= (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
+		pawnCount = Long.bitCount(wp & EngineConstants.FILE_C);
+		eval -= (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
+		pawnCount = Long.bitCount(wp & EngineConstants.FILE_D);
+		eval -= (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
+		pawnCount = Long.bitCount(wp & EngineConstants.FILE_E);
+		eval -= (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
+		pawnCount = Long.bitCount(wp & EngineConstants.FILE_F);
+		eval -= (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
+		pawnCount = Long.bitCount(wp & EngineConstants.FILE_G);
+		eval -= (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
+		pawnCount = Long.bitCount(wp & EngineConstants.FILE_H);
+		eval -= (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
+		
+		pawnCount = Long.bitCount(bp & EngineConstants.FILE_A);
+		eval += (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
+		pawnCount = Long.bitCount(bp & EngineConstants.FILE_B);
+		eval += (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
+		pawnCount = Long.bitCount(bp & EngineConstants.FILE_C);
+		eval += (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
+		pawnCount = Long.bitCount(bp & EngineConstants.FILE_D);
+		eval += (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
+		pawnCount = Long.bitCount(bp & EngineConstants.FILE_E);
+		eval += (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
+		pawnCount = Long.bitCount(bp & EngineConstants.FILE_F);
+		eval += (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
+		pawnCount = Long.bitCount(bp & EngineConstants.FILE_G);
+		eval += (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
+		pawnCount = Long.bitCount(bp & EngineConstants.FILE_H);
+		eval += (pawnCount - 1) * PENALTY_DOUBLED_PAWN;
+		
+		eval += (EngineConstants.WHITE_PAWN_V 	* (wpCount - bpCount)) ;
+		
+		return eval;
+	}
+	
 }
