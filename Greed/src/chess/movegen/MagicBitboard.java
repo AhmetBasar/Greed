825d726c1347f98979f1da293626d6616c5e7736
AhmetBasar <1989ahmetbasar@gmail.com>
2019-11-17 14:40:59 +0300

    Magic Bitboards Implementation.

--- Greed/src/chess/movegen/MagicBitboard.java
+++ Greed/src/chess/movegen/MagicBitboard.java
@@ -29,10 +29,15 @@ public class MagicBitboard {
 	
 	private static long[] rookMasks = new long[64];
 	private static long[] bishopMasks = new long[64];
+	private static long[] rookShifts = new long[64];
+	private static long[] bishopShifts = new long[64];
 	
 	static {
 		generateRookMasks();
 		generateBishopMasks();
+		generateShifts();
+		long[][] bishopOccVariations = generateOccupancyVariations(bishopMasks);
+		long[][] rookOccVariations = generateOccupancyVariations(rookMasks);
 	}
 	
 	private static void generateRookMasks() {
@@ -82,6 +87,34 @@ public class MagicBitboard {
 			bishopMasks[s] = bishopMask;
 			
 		}
+	}
+	
+	private static void generateShifts() {
+		for (int i = 0; i < 64; i++) {
+			rookShifts[i] = 64 - Long.bitCount(rookMasks[i]);
+			bishopShifts[i] = 64 - Long.bitCount(bishopMasks[i]);
+		}
+	}
+	
+	private static long[][] generateOccupancyVariations(long[] masks) {
+		long[][] occVariations = new long[64][];
+		
+		for (int s = 0 ; s < 64 ; s++) {
+			int variationCount = (int)Utility.SINGLE_BIT[Long.bitCount(masks[s])];
+			occVariations[s] = new long[variationCount];
+			
+			for (int varIndex = 1 ; varIndex < variationCount ; varIndex++) {
+				long curMask = masks[s];
+				for (int i = 0 ; i < 32 - Integer.numberOfLeadingZeros(varIndex) ; i++) {
+					if ((Utility.SINGLE_BIT[i] & varIndex) != 0) {
+						occVariations[s][varIndex] |= Long.lowestOneBit(curMask);
+					}
+				}
+				curMask &= (curMask - 1);
+			}
+		}
+		
+		return occVariations;
 	}
 	
 }
