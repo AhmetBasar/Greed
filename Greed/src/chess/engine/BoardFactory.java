08c4a1ed19451b5041a6cf70d26766e43e20728b
AhmetBasar <1989ahmetbasar@gmail.com>
2020-08-01 20:42:12 -0700

    Restructuring.

--- Greed/src/chess/engine/BoardFactory.java
+++ Greed/src/chess/engine/BoardFactory.java
@@ -4,24 +4,13 @@ import java.util.List;
 
 public class BoardFactory {
 	
-	public static IBoard getInstance(SearchParameters sp) {
-		return new BoardV7(sp.getBitboard(), sp.getPieces(), sp.getEpT(),
-				sp.getCastlingRights(), sp.getFiftyMoveCounter(),
-				sp.getZobristKeyHistory(), sp.getSide());
-	}
-	
-	public static IBoard getInstance(long[] bitboard, byte[] pieces, int epT, byte[][] castlingRights,
+	public static IBoard getInstance2(long[] bitboard, byte[] pieces, int epT, byte[][] castlingRights,
 			int fiftyMoveCounter, List<Long> zobristKeyHistory, int side) {
 		return new BoardV7(bitboard, pieces, epT, castlingRights, fiftyMoveCounter, zobristKeyHistory, side);
 	}
 	
-	public static IBoard getInstance2(long[] bitboard, byte[] pieces, int epT, byte[][] castlingRights,
-			int fiftyMoveCounter, List<Long> zobristKeyHistory, int side) {
-		return new BoardV8(bitboard, pieces, epT, castlingRights, fiftyMoveCounter, zobristKeyHistory, side);
-	}
-	
 	public static IBoard getInstance2(SearchParameters sp) {
-		return new BoardV8(sp.getBitboard(), sp.getPieces(), sp.getEpT(),
+		return new BoardV7(sp.getBitboard(), sp.getPieces(), sp.getEpT(),
 				sp.getCastlingRights(), sp.getFiftyMoveCounter(),
 				sp.getZobristKeyHistory(), sp.getSide());
 	}
